f035473ba0f4042faea2d28fa82b204f23913c87
yojhanusa <yojhan.cardenas.mtorreo.usa.edu.co>
2021-11-07 22:59:29 -0500

    Ajustes Machine

--- js/Machine.js
+++ js/Machine.js
@@ -23,7 +23,7 @@ function traerInformacionMachines() {
         datatype:"JSON",
         success:function(response){
             console.log(response);
-            pintarRespuestaMachine(response);
+            pintarRespuestaMachines(response);
         }
     });
 }
@@ -31,16 +31,16 @@ function traerInformacionMachines() {
 function pintarRespuestaMachines(response) {
     let myTable = "<table class='ui center aligned celled table'>" + 
     "<thead><tr><th>NOMBRE</th><th>MARCA</th><th>AÑO</th><th>DESCRIPCION</th><th colspan='3'></th></tr></thead>";
-    for(i=0;i<respuesta.length;i++){
+    for(i=0;i<response.length;i++){
         myTable+="<tr>";
-        myTable+="<td>"+respuesta[i].name+"</td>";
-        myTable+="<td>"+respuesta[i].brand+"</td>";
-        myTable+="<td>"+respuesta[i].year+"</td>";
-        myTable+="<td>"+respuesta[i].description+"</td>";
-        myTable+="<td>"+respuesta[i].category.name+"</td>";
-        myTable+="<td> <button class='ui yellow button' onclick=' actualizar("+respuesta[i].id+")'>Actualizar</button></td>";
-        myTable+="<td> <button class='ui red button' onclick='cargarDatosMachine("+respuesta[i].id+")'>Editar</button></td>";
-        myTable+="<td> <button class='ui red button' onclick='borrar("+respuesta[i].id+")'>Borrar</button></td>";
+        myTable+="<td>"+response[i].name+"</td>";
+        myTable+="<td>"+response[i].brand+"</td>";
+        myTable+="<td>"+response[i].year+"</td>";
+        myTable+="<td>"+response[i].description+"</td>";
+        myTable+="<td>"+response[i].category.name+"</td>";
+        myTable+="<td> <button class='ui yellow button' onclick=' actualizar("+response[i].id+")'>Actualizar</button></td>";
+        myTable+="<td> <button class='ui red button' onclick='cargarDatosMachine("+response[i].id+")'>Editar</button></td>";
+        myTable+="<td> <button class='ui red button' onclick='borrar("+response[i].id+")'>Borrar</button></td>";
 
         myTable+="</tr>";
     }
@@ -49,18 +49,24 @@ function pintarRespuestaMachines(response) {
 }
 
 function cargarDatosMachine(id) {
+    console.log(id);
     $.ajax({
+        datatype:'json',
         url:"http://129.151.100.76:8080/api/Machine/" + id,
         type:"GET",
-        datatype:"JSON",
         success:function(response){
             console.log(response);
-            var item = response
-            $("#id").val(item.id);
-            $("#Mname").val(item.name.split("T")[0]);
+            var item = response;
+            console.log(item.name);
+            $("#id").val(item.id) ;
+            console.log(item.id);
+            $("#Mname").val(item.name);
+            console.log(item.name);
             $("#Mbrand").val(item.brand);
+            console.log(item.brand);
             $("#Myear").val(item.year);
             $("#Mdescription").val(item.description);
+            $("#select-category").val(item.category.id);
         },
         error: function(jqXHR, textStatus, errorThrown) {
             alert("Ha ocurrido un error desconocido");
@@ -69,8 +75,35 @@ function cargarDatosMachine(id) {
     });
 }
 
+
+/*
+function cargarDatosMachine(id) {
+    
+    $.ajax({
+        datatype:"JSON",
+        url:"http://129.151.100.76:8080/api/Machine/" + id,
+        type:"GET",
+        success:function(response){
+            console.log(response);
+            var item = response
+            $("#id").val(item.id);
+            $("#Mname").val(item.name);
+            $("#Mbrand").val(item.brand);
+            $("#Myear").val(item.year);
+            $("#Mdescription").val(item.description);
+            $("#select-category").val(item.category.id);
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("Ha ocurrido un error desconocido");
+    
+        }
+    });
+    
+}
+*/
+
 function guardarMachine() {
-    if ($("#startDate").val().length == 0 || $("#devolutionDate").val().length == 0 || $("#status").val().length == 0) {
+    if ($("#Mname").val().length == 0 || $("#Mbrand").val().length == 0 || $("#Myear").val().length == 0 || $("#Mdescription").val().length == 0) {
         alert("Todos los campos son obligatorios")
     } else {
         let elemento = {
@@ -111,8 +144,74 @@ function guardarMachine() {
             }
             });
 }
+}
 
 
-function actualizar(params) {
+function actualizar (idElemento) {
+    if ($("#Mname").val().length == 0 || $("#Mbrand").val().length == 0 || $("#Myear").val().length == 0 || $("#Mdescription").val().length == 0) {
+        alert("Todos los campos son obligatorios")
+    } else {
+        let elemento = {
+            id:idElemento,
+            name: $("#Mname").val(),
+            brand: $("#Mbrand").val(),
+            year: $("#Myear").val(),
+            description: $("#Mdescription").val(),
+            category:{ id: + $("#select-category").val()},
+        }
+        console.log(elemento);
+        let dataToSend = JSON.stringify(elemento);
+
+        $.ajax({
+            url:"http://129.151.100.76:8080/api/Machine/update",
+            type:"PUT",
+            data:dataToSend,
+            contentType:"application/JSON",
+            success:function(response){
+                console.log(Response);
+                $("#miListaMachines").empty();
+                traerInformacionMachines();
+                alert("Maquina actualziada correctamente ✔")
+                $("#miListaMachines").empty();
+                $("#id").val("");
+                $("#Mname").val("");
+                $("#Mbrand").val("");
+                $("#Myear").val("");
+                $("#Mdescription").val("");
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                alert("No se actualizo correctamente");
+        
+            }
+
+        });
+    }
+}
+
+function borrar(idElemento) {
+    var elemento = {
+        id:idElemento
+    }
+
+    var dataToSend = JSON.stringify(elemento);
     
+    console.log(dataToSend);
+
+    $.ajax({
+        url:"http://129.151.100.76:8080/api/Machine/" + idElemento,
+        type:"DELETE",
+        datatype:"json",
+        data:dataToSend,
+        contentType:"application/JSON",
+
+        success:function(response){
+            console.log(response);
+            $("#miListaMachines").empty();
+            alert("Maquina eliminada correctamente 🗑");
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+            alert("No se elimino correctamente");
+    
+        }
+        });
 }
